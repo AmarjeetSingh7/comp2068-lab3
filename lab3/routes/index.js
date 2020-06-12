91b172ccfbfc4c26717df75e38a3969417851142
AmarjeetThind <amarjeetthind7@gmail.com>
2020-06-11 19:26:48 -0500

    Add project files.

---
+++ lab3/routes/index.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/Amarjeet', function (req, res) {
+    res.render('Amarjeet', { title: 'Express' });
+});
+
+router.get('/Dad', function (req, res) {
+    res.render('Dad', { title: 'Express' });
+});
+
+
+router.get('/Mom', function (req, res) {
+    res.render('Mom', { title: 'Express' });
+});
+
+
+
+module.exports = router;
